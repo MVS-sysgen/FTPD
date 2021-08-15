68f4f854ba2e286314e87dddfc6bbcb8b33a8233
mainframed <mainframed767@gmail.com>
2021-08-14 17:06:27 -0700

    moving files and creating better file structure

--- build_01.sh
+++
@@ -1,4 +0,0 @@
-echo "Generating 01_assemble_ftp_objects.ebcdic.jcl"
-cd $(dirname $0)
-rdrprep build_01_assemble_ftp_objects.template build_01_assemble_ftp_objects.ebcdic.jcl
-echo "Done"
--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ echo "Generating Random FTPD password"
 cd $(dirname $0)
 
 password=$(cat /dev/urandom 2>/dev/null| tr -dc 'A-Z0-9$@#' 2>/dev/null | head -c 8 )
-echo "Using: $password"
+#echo "FTPD User password: $password"
 sed "s/12345678/$password/" 01_update_rakf.template > 01_update_rakf.jcl
+sed -e '/###FTPD.CONF###/ {' -e 'r FTPD.conf' -e 'd' -e '}' 02_install_ftpd.template > 02_install_ftpd.jcl
 
 echo "Done"
